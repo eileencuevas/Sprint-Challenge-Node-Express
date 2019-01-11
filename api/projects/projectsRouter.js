be18c8f199033d92f531d7da926c2e7f9849136c
steelworx <eileencuevas62@gmail.com>
2019-01-11 12:37:08 -0500

    added PUT endpoint in projectsRouter.js

--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -65,11 +65,36 @@ router.post('/', (req, res) => {
                 res.status(201).json(newProject);
             })
             .catch(() => {
-                res.status(500).json({ "error": 'No information could be retrieved. '});
+                res.status(500).json({ "error": `The project couldn't be added. Please try again. `});
             });
     } else {
         res.status(400).json({ "error": 'Please include a name and description.' });
     }
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const dataForUpdate = req.body;
+
+    projectModel
+    .get(id)
+    .then(project => {
+        if (project) {
+            projectModel
+                .update(id, dataForUpdate)
+                .then(updatedProject => {
+                    res.status(200).json(updatedProject);
+                })
+                .catch(() => {
+                    res.status(500).json({ "error": `The project couldn't be updated. Please try again. `});
+                });
+        } else {
+            res.status(404).json({ "error": 'No project found with the specified ID.' });
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ "error": 'No information could be retrieved. '});
+    });
 })
 
 module.exports = router;
